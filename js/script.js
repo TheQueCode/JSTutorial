9eb96b3b24b50ddaca4acfe6669e54e80b2b2e04
TheQueCode <thequecode@gmail.com>
2025-06-28 15:00:54 -0500

    querySelector

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ para.textContent = 'This is my paragraph.'
 
 title.appendChild(para);
 container.appendChild(title);
+
+let fullName = 'Que Code';
+let age = 50;
+let student = true;
+
+document.querySelector('#p1').textContent = `My name is: ${fullName}`;
+document.querySelector('#p2').textContent = `My age is: ${age}`;
+document.querySelector('#p3').textContent = `I am a student? ${student}`;
