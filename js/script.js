67cfc7d6d9d89863789414ac4bc48110bef527ee
TheQueCode <thequecode@gmail.com>
2025-06-29 23:16:49 -0500

    random lotto number gen

--- js/script.js
+++ js/script.js
@@ -127,8 +127,103 @@ function reset () {
   count = 0;
   label.textContent = count;
 }
-*/
+
 // Math object
 
+console.log(Math.PI);
+console.log(Math.E);
 
+let x = 3.21;
+let y = 2;
+let z;
 
+const arr = [124, 333, 55, 6, -700, 888, 9, 1110];
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x); // 3
+z = Math.pow(x, y);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+
+let max = Math.max(...arr);
+let min = Math.min(...arr);
+
+console.log(max);
+console.log(min);
+
+console.log(z);
+
+//Random number
+
+const min = 50;
+const max = 100;
+
+const randomNum = Math.floor(Math.random() * (max - min) + min);
+
+console.log(randomNum);
+*/
+// random number generator
+
+// Lucky for Life
+// 5 balls(1 to 48)
+// Lucky Ball(1 to 18)
+
+function luckyTimes () {
+  let output;
+  let luckyNums = [];
+  for (let i = 0; i < 5; i++){
+    output = Math.floor(Math.random() * 48 + 1);
+    luckyNums.push(output);
+  }
+  let luckyBall;
+  luckyBall = Math.floor(Math.random() * 18 + 1);
+  luckyNums.push(luckyBall);
+  console.log(luckyNums);
+};
+
+luckyTimes();
+
+// Powerball
+// 5 balls(1 to 69)
+// Powerball(1 to 26)
+
+function powerBallTimes () {
+  let output;
+  let powerNums = [];
+  for (let i = 0; i < 5; i++){
+    output = Math.floor(Math.random() * 69 + 1);
+    powerNums.push(output);
+  }
+  let powerBall;
+  powerBall = Math.floor(Math.random() * 26 + 1);
+  powerNums.push(powerBall);
+  console.log(powerNums);
+};
+
+powerBallTimes();
+
+// Mega Millions
+// 5 balls(1 to 70)
+// Mega Ball(1 to 24)
+
+function megaTimes () {
+  let output;
+  let megaNums = [];
+  for (let i = 0; i < 5; i++){
+    output = Math.floor(Math.random() * 70 + 1);
+    megaNums.push(output);
+  }
+  let megaBall;
+  megaBall = Math.floor(Math.random() * 24 + 1);
+  megaNums.push(megaBall);
+  console.log(megaNums);
+};
+
+megaTimes();
